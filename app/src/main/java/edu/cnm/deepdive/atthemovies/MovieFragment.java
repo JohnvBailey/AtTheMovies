3b2ab8defcaba157da189bd798019e3bf0ad0e45
John B <bohnjailey56@gmail.com>
2019-06-11 15:54:10 -0600

    This is Broken

--- app/src/main/java/edu/cnm/deepdive/atthemovies/MovieFragment.java
+++ app/src/main/java/edu/cnm/deepdive/atthemovies/MovieFragment.java
@@ -1,23 +1,18 @@
 package edu.cnm.deepdive.atthemovies;
 
 
-import android.content.Context;
-import android.os.Bundle;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.navigation.Navigation;
-import edu.cnm.deepdive.atthemovies.model.Movie;
-import edu.cnm.deepdive.atthemovies.viewmodel.MoviesViewModel;
+    import android.content.Context;
+    import android.os.Bundle;
+    import android.widget.*;
+    import androidx.annotation.NonNull;
+    import androidx.fragment.app.Fragment;
+    import android.view.LayoutInflater;
+    import android.view.View;
+    import android.view.ViewGroup;
+    import androidx.lifecycle.ViewModelProviders;
+    import androidx.navigation.Navigation;
+    import edu.cnm.deepdive.atthemovies.model.Movie;
+    import edu.cnm.deepdive.atthemovies.viewmodel.MoviesViewModel;
 
 
 /**
@@ -46,31 +41,40 @@ public class MovieFragment extends Fragment {
 
     ListView moviesListView = view.findViewById(R.id.movies_list);
 
-    final MoviesViewModel viewModel = ViewModelProviders.of(this).get(MoviesViewModel.class);
+    final MoviesViewModel viewModel = ViewModelProviders.of(getActivity()).get(MoviesViewModel.class);
 
     final ArrayAdapter<Movie> adapter = new ArrayAdapter<Movie>(context, android.R.layout.simple_list_item_1,
         viewModel.getMovies());
     moviesListView.setAdapter(adapter);
+
+    final Spinner genreSpinner = view.findViewById(R.id.new_movie_genre);
+    ArrayAdapter<Movie.Genre> genreAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, Movie.Genre.values());
+    genreSpinner.setAdapter(genreAdapter);
 
     Button newMovieButton = view.findViewById(R.id.new_movie_button);
     newMovieButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
         EditText newMovieNameEditText = view.findViewById(R.id.new_movie_name);
+        EditText newMovieScreenwriter = view.findViewById(R.id.new_movie_screenwriter);
         Movie newMovie = new Movie();
         newMovie.setTitle(newMovieNameEditText.getText().toString());
+        newMovie.setScreenwriter(newMovieScreenwriter.getText().toString());
+        newMovie.setGenre((Movie.Genre) genreSpinner.getSelectedItem());
         viewModel.addMovie(newMovie);
-        adapter.notifyDataSetChanged();
+        adapter.clear();
+        adapter.addAll(viewModel.getMovies());
         newMovieNameEditText.setText("");
       }
     });
-    moviesListView.setOnItemClickListener(new OnItemClickListener() {
+
+    moviesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
       @Override
       public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-       //this code open the actors fragment and passes the movie id that was clicked
+        //This code opens the actor fragment and passes the movie id that was clicked
         MovieFragmentDirections.ActionMovieFragmentToActorsFragment action =
             MovieFragmentDirections.actionMovieFragmentToActorsFragment()
-                .setMovie(adapter.getItem(position).getId());
+                .setMovieId(adapter.getItem(position).getId());
         Navigation.findNavController(view).navigate(action);
       }
     });
--- app/src/main/java/edu/cnm/deepdive/atthemovies/model/Movie.java
+++ app/src/main/java/edu/cnm/deepdive/atthemovies/model/Movie.java
@@ -2,10 +2,26 @@ package edu.cnm.deepdive.atthemovies.model;
 
 import androidx.annotation.NonNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Movie {
+
   private static Long last_id = 0L;
+
   private Long id;
+
   private String title;
+
+  private String screenwriter;
+
+  public enum Genre{
+    HORROR, ACTION, ROMCOM, DOCUMENTARY, ANIME, SCIFI, FANTASY
+  }
+
+  private Genre genre;
+
+  private List<Actor> actors = new ArrayList<>();
 
   public Movie() {
     id = ++last_id;
@@ -15,22 +31,41 @@ public class Movie {
     return id;
   }
 
-
-
-
   public String getTitle() {
     return title;
   }
 
   public void setTitle(String title) {
     this.title = title;
+  }
 
+  public List<Actor> getActors() {
+    return actors;
+  }
+
+  public void setActors(List<Actor> actors) {
+    this.actors = actors;
+  }
+
+  public String getScreenwriter() {
+    return screenwriter;
+  }
+
+  public void setScreenwriter(String screenwriter) {
+    this.screenwriter = screenwriter;
+  }
+
+  public Genre getGenre() {
+    return genre;
+  }
+
+  public void setGenre(Genre genre) {
+    this.genre = genre;
   }
 
   @NonNull
   @Override
   public String toString() {
-    return title;
+    return title + ": " + genre ;
   }
 }
-
